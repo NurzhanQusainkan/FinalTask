f71c6f13051cbc0ef6f5076a6eb504b4a9c8dd1e
Nurzhan  Qusainkan <qusainkan@MacBook-Air-Nurzhan-2.local>
2024-01-25 14:02:59 +0600

    step_2

--- Program.cs
+++ Program.cs
@@ -5,5 +5,47 @@
     return arraySimvol;
 }
 
+string[] GetArrayThreeSimvol (string[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        string simvol = array[i];
+        if (simvol.Length <= 3)
+        {
+            count++;
+        }
+    }
+    string[] arrayThreeSimvol = new string[count];
+    for (int i = 0, j = 0; i < array.Length; i++)
+    {
+        string simvol = array[i];
+        if (simvol.Length <= 3)
+        {
+            arrayThreeSimvol[j] = simvol;
+            j++;
+        }
+    }
+    return arrayThreeSimvol;
+}
 
 
+void Print (string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.WriteLine();
+}
+
+Console.Write("Введите набор символов через знак (,) - ");
+string inStringSimvol = Console.ReadLine();
+string[] arraySimvol = GetArrayStringConsole(inStringSimvol);
+Console.WriteLine();
+Console.Write("Введенные символы - ");
+Print(arraySimvol);
+Console.WriteLine();
+Console.Write("Введенные символы длинной меньше либо равны 3 - ");
+Print(GetArrayThreeSimvol(arraySimvol));
+Console.WriteLine();
